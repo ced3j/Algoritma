99aeafc6dce7cf6be460bcd8d47449929885593b
deneme <spoonnoisthere@gmail.com>
2022-08-25 13:03:23 +0300

    Kod güncelleme

--- easy.js
+++ easy.js
@@ -1,4 +1,9 @@
 import { userValidation } from "./easy/1_userValidation.js";
 
-console.log(userValidation("u__hello_world123"));
+// console.log(userValidation("u__hello_world123"));
 
+
+
+import { longestWord } from "./easy/2_longestWord.js";
+
+console.log(longestWord("fun&!! time"));
